662c6ae0392fe8538dcf72f7861f8a747308ef6c
CarlosIvan45 <122888081+CarlosIvan45@users.noreply.github.com>
2026-02-28 15:04:06 -0600

    Primera base de la app HalliPlanner Ivan 28/02/2026

--- app/src/main/java/com/example/halliplanner/MainActivity.kt
+++ app/src/main/java/com/example/halliplanner/MainActivity.kt
@@ -5,16 +5,34 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
-}
+  class MainActivity : AppCompatActivity() {
+
+      override fun onCreate(savedInstanceState: Bundle?) {
+          super.onCreate(savedInstanceState)
+          setContentView(R.layout.activity_main)
+
+          val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+
+          replaceFragment(HomeFragment())
+
+          bottomNav.setOnItemSelectedListener {
+
+              when(it.itemId) {
+                  R.id.nav_home -> replaceFragment(HomeFragment())
+                  R.id.nav_schedule -> replaceFragment(ScheduleFragment())
+                  R.id.nav_tasks -> replaceFragment(TasksFragment())
+                  R.id.nav_profile -> replaceFragment(ProfileFragment())
+              }
+              true
+          }
+      }
+
+      private fun replaceFragment(fragment: Fragment) {
+          supportFragmentManager.beginTransaction()
+              .replace(R.id.fragment_container, fragment)
+              .commit()
+      }
+  }
